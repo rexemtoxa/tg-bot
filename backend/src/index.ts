6489bc9016ceb32f407dca72afe2f564a5e34057
Anton Rehemae <a.rehemae@blocktrade.com>
2024-06-09 11:39:05 +0300

    Add ssl required

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ const client = postgres({
   database: process.env.DB_DATABASE,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: false
+  ssl: 'require'
 });
 
 const SALT = process.env.SALT || 'somesalt';
